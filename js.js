28c4150cb758bd92d3942747931bfa24ad3d4b99
wol03 <1251452041a@gmail.com>
2024-02-05 13:25:58 +0100

    cambio

--- js.js
+++ js.js
@@ -23,7 +23,7 @@ function movil(){
    
    document.getElementById("icono1").style.marginLeft = "80%" ;
    
-   document.getElementById("buscar").style.height = "60%";
+   document.getElementById("buscar").style.height = "80%";
    
    document.getElementById('pasea').style.width = "33%";
    
